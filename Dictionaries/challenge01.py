64173ade05441490d4c8a6eaccd526c8030297ac
vitorbotinhao <jiroumaru.game@gmail.com>
2017-09-04 18:49:14 +0000

    new challenge with dictionaries

---
+++ Dictionaries/challenge01.py
@@ -0,0 +1,55 @@
+locations = {0: "You are sitting in front of a computer learning Python",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a small stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in the forest"}
+             
+#exits = [{"Q": 0},
+#         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+#         {"N": 5, "Q": 0},
+#         {"W": 1, "Q": 0},
+#         {"N": 1, "W": 2, "Q": 0},
+#         {"W": 2, "S": 1, "Q": 0}]
+         
+exits = {0: {"Q": 0},
+         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         2: {"N": 5, "Q": 0},
+         3: {"W": 1, "Q": 0},
+         4: {"N": 1, "W": 2, "Q": 0},
+         5: {"W": 2, "S": 1, "Q": 0}}
+         
+         
+vocabulary = {
+    "QUIT": "Q",
+    "NORTH": "N",
+    "SOUTH": "S",
+    "EAST": "E",
+    "WEST": "W"
+}
+
+loc = 1
+
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+    
+    print(locations[loc])
+    
+    if loc == 0:
+        break
+    
+    direction = input("Available exits are " + availableExits + " ").upper()
+    
+    if len(direction) > 1:
+       words = direction.split()
+       for word in words:
+           if word in vocabulary:
+               direction = vocabulary[word]
+               break;
+    
+    print()
+    
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannnot go in that direction")
